2c2204e1a604843f8a4fa62c574ea2a52d7f7134
ozkrpy <ozkrpy@gmail.com>
2016-05-13 18:24:45 -0400

    Agregar valores de ajuste

--- WorkFlowManager/app/src/main/java/org/ruffineo/workflowmanager/PreferencesActivity.java
+++ WorkFlowManager/app/src/main/java/org/ruffineo/workflowmanager/PreferencesActivity.java
@@ -1,22 +1,46 @@
 package org.ruffineo.workflowmanager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class PreferencesActivity extends AppCompatActivity {
 
     private static final String TAG = "AJUSTES_LOG";
+    private EditText editTextIPConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
         escribeLog("onCreate de ajustes");
+        editTextIPConfig = (EditText) findViewById(R.id.editText_IP);
+        editTextIPConfig.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                SharedPreferences prefs = getSharedPreferences("WorkFlowPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("ipNumber", editTextIPConfig.toString());
+                editor.commit();
+            }
+        });
+
     }
 
     private void escribeLog(String texto) {
