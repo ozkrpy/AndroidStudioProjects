80d5a362690d85197ee6cc03852e3b9095042e1b
ozkrpy <ozkrpy@gmail.com>
2016-06-09 08:34:31 -0400

    Fuel Watcher

---
+++ FuelWatcher/app/src/main/java/com/example/ozkrp/fuelwatcher/RecargaActivity.java
@@ -0,0 +1,13 @@
+package com.example.ozkrp.fuelwatcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recarga);
+    }
+}
